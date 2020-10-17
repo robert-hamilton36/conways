2272f5a53ccf2a59c1358bc256228c51ae74f11c
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-10-17 17:45:43 +1300

    add create board function

--- createBoard.js
+++ createBoard.js
@@ -1,5 +1,15 @@
 function createBoard (size) {
+    let board = []
+    for(let i = 0; i<size; i++){
+        board[i] = []
+        for(let j = 0; j<size; j++){
+            board[i][j] = 0
+        }
+    }
+    return board
 
 }
+
+createBoard (2) 
 
 module.exports = createBoard
