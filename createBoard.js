67f5ffac790f0d01259e222b3e366bc188dc8889
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-10-18 12:11:19 +1300

    not sure don't know how to make it goaway

--- createBoard.js
+++ createBoard.js
@@ -1,15 +1,37 @@
 function createBoard (size) {
+
+    // let board = [];
+    // for (let x = 0; x < size; x++) {
+    //   let rows = [];
+    //   for (let y = 0; y < size; y++) {
+    //     rows.push(true);
+    //   }
+    //   board.push(rows);
+    //   //console.log(board + "//////////")
+    // }
+    // console.log(board)
+  
+    // return board;
+
+
+
+
+
+
+
+
     let board = []
     for(let i = 0; i<size; i++){
-        board[i] = []
+        rowi = []
         for(let j = 0; j<size; j++){
-            board[i][j] = 0
+            rowi.push(0)
         }
+        board.push(rowi)
     }
     return board
 
 }
 
-createBoard (2) 
+// console.log(createBoard(2)) 
 
 module.exports = createBoard
