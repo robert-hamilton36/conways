fa7bae44eb20c85c1ebf157f97e06a00694ba4f9
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-10-18 12:09:11 +1300

    add indicesAreOutOfBounds

--- indicesAreOutOfBounds.js
+++ indicesAreOutOfBounds.js
@@ -1,6 +1,14 @@
 const isOutOfBounds = require('./isOutOfBounds')
 
 function indicesAreOutOfBounds (rowIndex, columnIndex, array) {
+    // if(isOutOfBounds(rowIndex, array)){
+    //     return true
+    // }else if(isOutOfBounds(columnIndex, array)){
+    //     return true
+    // }
+    // return false
+
+    return(isOutOfBounds(rowIndex, array) || isOutOfBounds(columnIndex, array))
 
 }
 
