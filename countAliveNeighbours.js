7f882012b4eea327c967b0263da0c7210e0e280b
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-10-18 12:28:57 +1300

    add countAliveNeighbours

--- countAliveNeighbours.js
+++ countAliveNeighbours.js
@@ -1,6 +1,26 @@
 const getNeighbours = require('./getNeighbours')
 
 function countAliveNeighbours (cellRow, cellColumn, board) {
+    //yuck
+    // return getNeighbours(cellRow, cellColumn, board).reduce((x, i) => {if(i){x++} return x},0)
+
+    //yuck twice
+    // return getNeighbours(cellRow, cellColumn, board).reduce(function(x, i){
+    //     if(i){
+    //         x++
+    //     }
+    //     return x
+    // },0)
+
+    let arr = getNeighbours(cellRow, cellColumn, board)
+    let count = 0
+    for(x of arr){
+        if(x){
+            count+=1
+        }
+    }
+
+    return count
 
 }
 
