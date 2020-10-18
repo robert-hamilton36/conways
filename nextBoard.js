2f516ba435dd87c9ece3417c320614bd730f1c78
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-10-18 13:46:04 +1300

    add nextBoard

--- nextBoard.js
+++ nextBoard.js
@@ -2,7 +2,29 @@ const nextCellState = require('./nextCellState')
 const countAliveNeighbours = require('./countAliveNeighbours')
 
 function nextBoard (currentBoard) {
+//for cell in board
+//count alive neighbours
+//get next board cell state
+    let nextBoard = []
 
+    
+
+    console.log(currentBoard.length)
+    for(var i = 0; i < currentBoard.length; i++){
+        nextBoard[i] = []
+        for(var j = 0; j< currentBoard[i].length; j++){
+            let currentCellState = false
+            if(currentBoard[i][j]){
+                currentBoard[i][j] = true
+            }
+            let neighbourCount = countAliveNeighbours(i, j, currentBoard)
+            nextBoard[i][j] = nextCellState(currentCellState, neighbourCount)
+        }
+    }
+
+
+
+    return nextBoard
 }
 
 module.exports = nextBoard
