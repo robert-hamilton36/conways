d50a9f5bd3f923d42448f8f9d62715081d5eabc0
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-10-18 13:05:09 +1300

    add nextCellState

--- nextCellState.js
+++ nextCellState.js
@@ -3,7 +3,11 @@ const isUnderPopulated = require('./isUnderPopulated')
 const isRessurectable = require('./isRessurectable')
 
 function nextCellState (cellState, neighbourCount) {
-
+    if(!cellState){
+        return isRessurectable(neighbourCount)
+    }
+    return (!(isOverPopulated(neighbourCount) || isUnderPopulated(neighbourCount)))
 }
+
 
 module.exports = nextCellState
