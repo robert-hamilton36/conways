3e8b10da9189a01cf8adbedd70927d3d5a36b564
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-10-18 14:21:52 +1300

    not sure what changed

--- displayBoard.js
+++ displayBoard.js
@@ -1,7 +1,4 @@
 function displayBoard (board) {
-  // eslint-disable-next-line no-console
-  process.stdout.write('\x1B[2J\x1B[0f')
-
   for(row of board){
     let disRow = []
     for (col of row){
